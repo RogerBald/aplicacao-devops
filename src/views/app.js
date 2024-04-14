f04dbe3c7066f8dbfd1e989713d1639791896728
Vinicyus <vinicyusads11@gmail.com>
2024-04-14 18:17:33 -0300

    trocando req e res para request e response

--- src/views/app.js
+++ src/views/app.js
@@ -4,14 +4,13 @@ const port = 3000;
 
 app.use(express.static(__dirname)); // Define o diretório atual como o diretório raiz para servir arquivos estáticos
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: __dirname }); // Envia o arquivo 'index.html' como resposta para solicitações na raiz
+app.get('/', (_request, response) => {
+    response.sendFile('index.html', { root: __dirname }); // Envia o arquivo 'index.html' como resposta para solicitações na raiz
 });
 
-app.get('/integrantes', (req, res) => {
-    res.sendFile('integrantes.html', { root: __dirname }); // Envia o arquivo 'integrantes.html' como resposta para solicitações em '/integrantes'
+app.get('/integrantes', (_request, response) => {
+    response.sendFile('integrantes.html', { root: __dirname }); // Envia o arquivo 'integrantes.html' como resposta para solicitações em '/integrantes'
 });
-
 
 app.listen(port, () => {
     console.log(`Servidor está rodando na porta: ${port}`);
